ce52a70313052607dc91c258a07f206f2979dd65
Apparat Normalniy <2190055@gmail.com>
2021-10-06 18:12:18 +0300

    Scene settings

--- Assets/Scripts/MRTK/ObjectCreator.cs
+++ Assets/Scripts/MRTK/ObjectCreator.cs
@@ -1,8 +1,6 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using DataScripts;
-using Microsoft.MixedReality.Toolkit;
 using Microsoft.MixedReality.Toolkit.Input;
 using Serialization;
 using Storage;
